84e1938dbeeec71c4f4affccaad673df42c63cc8
Barrrox <matheusbarros173@gmail.com>
2026-01-17 17:27:39 -0300

    ADD: teste ler_input_interface

---
+++ Testes/teste_fileSystemManager.py
@@ -0,0 +1,62 @@
+import unittest
+
+from Classes.file_system_manager import FileSystemManager
+
+class TesteSystemFileManager(unittest.TestCase):
+
+    def setUp(self):
+
+        self.file_system_manager = FileSystemManager()
+        return 
+    
+    def tearDown(self):
+        return 
+    
+    # ler_input_interface
+    def test_comando_inexistente(self):
+
+        comandos_teste = [
+            "comando_inexistente",
+        ]
+
+        retorno_esperado = ("Comando inválido ou não existente! -2")
+
+        for comando_teste in comandos_teste:
+
+            retorno_real = self.file_system_manager.ler_input_interface(comando_teste)
+
+            # self.assertEqual(retorno_esperado, retorno_real)
+            self.assertEqual(retorno_esperado, retorno_real)
+
+    # ler_input_interface
+    def test_comando_invalido(self):
+
+        comandos_teste = [
+            "",
+            " ",
+            "  ",
+            "\n",
+        ]
+        retorno_esperado = ("Comando inválido ou não existente! -1")
+
+        for comando_teste in comandos_teste:
+
+            retorno_real = self.file_system_manager.ler_input_interface(comando_teste)
+
+            # self.assertEqual(retorno_esperado, retorno_real)
+            self.assertEqual(retorno_esperado, retorno_real)
+
+    # ler_input_interface
+    def test_comando_valido(self):
+
+        comando_teste = "exemplo katchau magrao123"
+        retorno_esperado = "Comando de exemplo! você escreveu 'exemplo' seguido de ('katchau', 'magrao123')."
+
+        # Chama a função
+        retorno_real = self.file_system_manager.ler_input_interface(comando_teste)
+
+        self.assertEqual(retorno_esperado, retorno_real)
+
+
+if __name__ == "__main__":
+    unittest.main()
