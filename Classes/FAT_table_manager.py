b3fd78bc0e0393232cc71557c344d0d3779ea062
Andre <andregustavof@hotmail.com>
2026-01-19 21:58:09 -0300

    FAT - alocar_arquivo

--- Classes/FAT_table_manager.py
+++ Classes/FAT_table_manager.py
@@ -6,13 +6,59 @@ class FAT_table_manager:
     def __init__(self):
         self.file_sys_manager = FileSystemManager()
 
-
-    def alocar_arquivo(tamanho_arquivo):
-
-        tamanho_cluster = FileSystemManager.get_tamanho_cluster
+    def get_entrada_FAT(self, arquivo, numero_entradas): # pega a posição absoluta de uma entrada FAT livre
+        offset_fat = self.file_sys_manager.get_offset("fat1")
         
-        quantidade_de_clusters = math((tamanho_arquivo/tamanho_cluster)).ceil
+        entradas = []
+        with open(arquivo, 'r+b') as f:
+            f.seek(offset_fat)
 
+            posicao = 0
+
+            for i in range(numero_entradas):
+                f.seek(posicao)
+                entrada_bytes = f.read(4)  # Lê 4 bytes (32 bits) da entrada FAT
+                
+                if entrada_bytes != 0x00000000:
+                    posicao = f.tell()
+                else:
+                    entradas.append(posicao)
+            
+        return entradas
+
+
+    def alocar_arquivo(self, tamanho_arquivo, arquivo):
+
+        # falta alterar o campo 1° cluster da entrada do arquivo no root dir
+
+        tamanho_cluster = self.file_sys_manager.get_tamanho_cluster()
+
+        quantidade_de_clusters = math.ceil(tamanho_arquivo/tamanho_cluster)
+        clusters_alocados = []
+
+        if self.procurar_cluster_livre():
+            
+            with open(arquivo, 'r+b') as f:
+                clusters_alocados.append(self.procurar_cluster_livre())
+
+                for i in range(quantidade_de_clusters-1):
+                    cluster = self.procurar_cluster_livre()
+                    clusters_alocados.append(cluster)
+
+                # pega as entradas livres necessárias na tabela FAT
+                entradas = self.get_entrada_FAT(arquivo, quantidade_de_clusters) 
+
+                for i in range(len(entradas)):
+                    posicao = entradas[i]
+                    f.seek(posicao)
+
+                    if i == len(entradas)-1:
+                        valor = 0xFFFFFFFF  # EOF
+                        f.write(valor.to_bytes(4, byteorder='little'))
+                    else:
+                        valor = entradas[i]
+                        f.write(valor.to_bytes(4, byteorder='little'))
+                        
         # aloca os clusters dos arquivos na tabela
         return
     
@@ -20,7 +66,7 @@ class FAT_table_manager:
         # desaloca os clusters de um arquivo alocado na tabela
         return
 
-    def procurar_entrada_livre():
+    def procurar_cluster_livre(self):
         # procura um cluster livre
         return
 
@@ -31,3 +77,4 @@ class FAT_table_manager:
     def sincronizar_fat(primeiro_cluster):
         # Sincroniza as tabelas FAT
         return
+    
--- Classes/data_manager.py
+++ Classes/data_manager.py
@@ -1,4 +1,13 @@
 class data_manager:
+
+    def procurar_cluster_livre():
+
+        return
+    
     def alocar_cluster():
+
+        return
+    
+    def liberar_cluster():
 
         return
