ed1556549025a8b7c8303c4c61cadc84ce45a235
Andre <andregustavof@hotmail.com>
2026-01-20 22:56:36 -0300

    data manager -> procurar cluster livre

--- Classes/FAT_table_manager.py
+++ Classes/FAT_table_manager.py
@@ -1,10 +1,12 @@
 import math
 from Classes.file_system_manager import FileSystemManager
+from Classes.data_manager import data_manager
 
 class FAT_table_manager:
 
     def __init__(self):
         self.file_sys_manager = FileSystemManager()
+        self.data_manager = data_manager()
 
     def get_entrada_FAT(self, arquivo, numero_entradas): # pega a posição absoluta de uma entrada FAT livre
         offset_fat = self.file_sys_manager.get_offset("fat1")
@@ -34,40 +36,35 @@ class FAT_table_manager:
         tamanho_cluster = self.file_sys_manager.get_tamanho_cluster()
 
         quantidade_de_clusters = math.ceil(tamanho_arquivo/tamanho_cluster)
-        clusters_alocados = []
 
-        if self.procurar_cluster_livre():
-            
+        clusters_alocados = [] # separa a quantia de clusters livres necessários para o arquivo
+        clusters_alocados = self.data_manager.procurar_cluster_livre(quantidade_de_clusters)
+       
+        if clusters_alocados: # se tem clusters livres 
+    
             with open(arquivo, 'r+b') as f:
-                clusters_alocados.append(self.procurar_cluster_livre())
-
-                for i in range(quantidade_de_clusters-1):
-                    cluster = self.procurar_cluster_livre()
-                    clusters_alocados.append(cluster)
-
+                
                 # pega as entradas livres necessárias na tabela FAT
                 entradas = self.get_entrada_FAT(arquivo, quantidade_de_clusters) 
 
-                for i in range(len(entradas)):
+                for i in range(len(entradas)): # posiciona o cursor na posição absoluta da entrada FAT
                     posicao = entradas[i]
                     f.seek(posicao)
 
-                    if i == len(entradas)-1:
+                    if i == len(entradas)-1: # se for o final da chain marca como EOF
                         valor = 0xFFFFFFFF  # EOF
                         f.write(valor.to_bytes(4, byteorder='little'))
-                    else:
+                    else: # se nao for o final da chain aponta para o próximo cluster
                         valor = entradas[i]
                         f.write(valor.to_bytes(4, byteorder='little'))
+        else:
+            return print("Não há clusters livres suficientes para alocar o arquivo.")
                         
         # aloca os clusters dos arquivos na tabela
         return
     
     def desalocar_arquivo(primeiro_cluster):
         # desaloca os clusters de um arquivo alocado na tabela
-        return
-
-    def procurar_cluster_livre(self):
-        # procura um cluster livre
         return
 
     def pegar_proximo_cluster():
--- Classes/data_manager.py
+++ Classes/data_manager.py
@@ -1,8 +1,36 @@
+from Classes.file_system_manager import FileSystemManager
+from Formatador.formatador import Formatador
+
 class data_manager:
+    def __init__(self):
+        self.file_sys_manager = FileSystemManager()
+        self.formatador = Formatador()
+    
+    def procurar_cluster_livre(self, arquivo, n_clusters,):
+        posicao_clusters = []
+        offset = self.formatador.get_offset("area_dados")
 
-    def procurar_cluster_livre():
+        tamanho_cluster = self.file_sys_manager.get_tamanho_cluster()
+        tamanho_setor = self.file_sys_manager.get_bytes_por_setor()
+        setores_por_cluster = self.file_sys_manager.get_setores_por_cluster()
 
-        return
+
+        with open(arquivo, 'r+b') as f:
+            f.seek(offset)
+
+            for i in n_clusters: # para cada cluster necessário
+                estado_cluster = f.read(tamanho_cluster) # lê o tamaho de 1 cluster
+                if estado_cluster == b'\x00' * tamanho_cluster: # se todos os bytes forem 0, o cluster está livre
+                    posicao_clusters.append(offset + (i * tamanho_cluster) ) # salva a posição absoluta do cluster
+
+                else: # se não, como o f.read já desloca o cursor, basta pular para a próxima iteração
+                    pass  
+
+
+        if len(posicao_clusters) == n_clusters:
+            return posicao_clusters
+        else:
+            return None, print("Erro, não há clusters livres suficientes. Lista incompleta em anexo"), posicao_clusters
     
     def alocar_cluster():
 
