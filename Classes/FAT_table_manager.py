66e7df637160d9ee195a709eb3b82c5fd2dd99c6
Barrrox <matheusbarros173@gmail.com>
2026-01-25 01:56:30 -0300

    REFACTOR: usa posição relativa em vez de absoluta

--- Classes/FAT_table_manager.py
+++ Classes/FAT_table_manager.py
@@ -71,7 +71,12 @@ class FAT_table_manager:
 
         return entradas, None
 
-    def alocar_arquivo(self, tamanho_arquivo, arquivo):
+    def alocar_entradas_FAT(self, sistema, tamanho_arquivo) :
+        """
+        sistema: path absoluto para o sistema de arquivos
+        tamanho_arquivo: tamanho do arquivo a ser alocado
+        
+        """
 
         # falta alterar o campo 1° cluster da entrada do arquivo no root dir
 
@@ -79,37 +84,39 @@ class FAT_table_manager:
 
         quantidade_de_clusters = math.ceil(tamanho_arquivo/tamanho_cluster)
 
+        entradas, __Error = self.procurar_entradas_FAT(sistema, quantidade_de_clusters) 
+
+        offset_fat = self.file_sys_manager.get_offset("fat1")
+
         # separa a quantia de clusters livres necessários para o arquivo
-        clusters_alocados, __Error = self.procurar_clusters_livre(quantidade_de_clusters)
        
         if __Error == None: # se tem clusters livres 
     
-            with open(arquivo, 'r+b') as f:
+            with open(sistema, 'r+b') as f:
                 
                 # pega as entradas livres necessárias na tabela FAT
-                entradas = self.get_entrada_FAT(arquivo, quantidade_de_clusters) 
 
                 for i in range(len(entradas)): # posiciona o cursor na posição absoluta da entrada FAT
-                    posicao = entradas[i]
+
+                    posicao = offset_fat + (entradas[i] * 4)  # Cada entrada FAT tem 4 bytes
+                    posicao = posicao.to_bytes(4, byteorder='little')
                     f.seek(posicao)
 
                     if i == len(entradas)-1: # se for o final da chain marca como EOF
-                        valor = 0xFFFFFFFF  # EOF
-                        f.write(valor.to_bytes(4, byteorder='little'))
+                        final_cluster_chain = 0xFFFFFFFF  # EOF relativo ao sistema de arquivos, já em binário
+                        f.write(final_cluster_chain)
+
+                        break # sai do loop pois ja alocou todos os clusters necessários
+
                     else: # se nao for o final da chain aponta para o próximo cluster
-                        valor = entradas[i]
-                        f.write(valor.to_bytes(4, byteorder='little'))
+                        posicao_prox_cluster = offset_fat + (entradas[i+1] * 4)
+                        posicao_prox_cluster = posicao_prox_cluster.to_bytes(4, byteorder='little')
+                        
+                        f.write(posicao_prox_cluster)
         else:
             return print("Não há clusters livres suficientes para alocar o arquivo.")
                         
-        # aloca os clusters dos arquivos na tabela
-
-        # chama o root dir manager para alterar a entrada do arquivo no root dir
-        self.root_manager.escrever_entrada(arquivo, clusters_alocados[0]) # nao implementado
-
-        # chama o data manager para alocar os clusters no arquivo
-        self.data_manager.alocar_cluster(clusters_alocados, arquivo)
-        return
+        return entradas
 
     
     def desalocar_arquivo(primeiro_cluster):
@@ -120,7 +127,6 @@ class FAT_table_manager:
         # Encontra o proximo cluster de uma chain     
         return
 
-    def sincronizar_fat(primeiro_cluster):
+    def sincronizar_fat_1_2(primeiro_cluster):
         # Sincroniza as tabelas FAT
         return
-    
