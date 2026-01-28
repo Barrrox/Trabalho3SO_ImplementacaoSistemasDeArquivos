364ed787622498974094224e0625dfc887bea5c5
Barrrox <matheusbarros173@gmail.com>
2026-01-28 00:00:29 -0300

    FIX: estava transformando os bytes em uma lista

--- Classes/disk_manager.py
+++ Classes/disk_manager.py
@@ -24,7 +24,7 @@ class disk_manager:
         with open(endereco_particao, 'r+b') as f:
             f.seek(posicao)
 
-            lido = list(f.read(self.tamanho_setor))  # lê 1 setor # talvez acessar uma variavel da classe a cada leitura cause lentidão, não sei
+            lido = f.read(self.tamanho_setor)  # lê 1 setor # talvez acessar uma variavel da classe a cada leitura cause lentidão, não sei
         
         return lido
 
