341ac1a5485f737a0de10e90ea8736710fa87d79
Andre <andregustavof@hotmail.com>
2026-01-28 14:23:31 -0300

    FIX e REFACTOR: ler_setor()

--- Classes/disk_manager.py
+++ Classes/disk_manager.py
@@ -28,35 +28,21 @@ class disk_manager:
         
         return lido
 
-
-    # posicao == posição absoluta em bytes no arquivo
-    # dados == conjunto de dados completo a serem escritos naquela posição
     def escrever_setor(self, posicao, dados): 
+        """
+        Docstring for escrever_setor
+        
+        :param posicao: posicao de escrita no arquivo
+        :param dados: dados a serem escritos, deve ter tamanho de 1 setor
+        """
 
         endereco_particao = self.file_sys_manager.get_endereco_particao()
 
-        escritas = math.ceil(len(dados) / self.tamanho_setor) # quantia de setores a serem escritos
-        bytes_escritos = 0
         with open(endereco_particao, 'r+b') as f:
-            f.seek(posicao)
+            f.seek(posicao)   
+            f.write(dados)
 
-            for i in range(escritas):
-
-                inicio_escrita = i * self.tamanho_setor
-                fim_escrita = inicio_escrita + self.tamanho_setor
-
-                setor_a_escrever = dados[inicio_escrita:fim_escrita] # pega 1 setor dos byte_array dos dados para escrever
-
-                if len(setor_a_escrever) < self.tamanho_setor:
-                    # se o setor a escrever for menor que o tamanho do setor, completa com zeros
-                    setor_a_escrever += b'\x00' * (self.tamanho_setor - len(setor_a_escrever))
-
-                f.write(setor_a_escrever)
-                bytes_escritos += len(setor_a_escrever)
-
-            if bytes_escritos != len(dados):
-                print(f"Erro na escrita do setor. bytes_escritos != len(dados): {bytes_escritos} != {len(dados)}")
-            if bytes_escritos + len(dados) != f.tell():
-                print(f"Erro na escrita do setor. bytes_escritos + len(dados) != f.tell(): {bytes_escritos} + {len(dados)} != {f.tell()}")
-
-        return bytes_escritos
+            if f.tell == int(posicao) + self.tamanho_setor: # se escreveu certo, posicao inicial + tamanho de um setor é a mesma da posição de término
+                return True
+            else: # se não
+                return False
